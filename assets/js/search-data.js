8c7fde061db2d8f5dc4a05a6fadcd382e456b49d
rehan-muh <119552078+rehan-muh@users.noreply.github.com>
2026-03-05 10:07:31 +0000

    Deploying to gh-pages from @ rehan-muh/rehan-muh.github.io@e1c1f9d81ea6dfe37f4e47022612b5d419b1eefd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,14 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
